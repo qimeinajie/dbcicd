d187d81c7cf156d60990e6617ae85cdaaca58d0b
BaoZhe <175539358@qq.com>
2021-12-01 17:51:01 +0800

    ci cd

--- migration/db/changelogs/migration/DML/002_insert_data.sql
+++ migration/db/changelogs/migration/DML/002_insert_data.sql
@@ -4,4 +4,5 @@ INSERT INTO branch VALUES(1, 'User1',01);
 INSERT INTO branch VALUES(2, 'User2',02);
 INSERT INTO branch VALUES(3, 'User3',03);
 INSERT INTO branch VALUES(4, 'User4',04);
+INSERT INTO branch VALUES(5, 'User5',05);
 --rollback select 1;
