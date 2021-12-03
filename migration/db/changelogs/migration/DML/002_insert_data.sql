7f626e7a99702c99f8595d94ac54de62ed2df092
BaoZhe <175539358@qq.com>
2021-12-03 10:44:16 +0800

    update

--- migration/db/changelogs/migration/DML/002_insert_data.sql
+++ migration/db/changelogs/migration/DML/002_insert_data.sql
@@ -5,3 +5,8 @@ INSERT INTO branch VALUES(2, 'User2',02);
 INSERT INTO branch VALUES(3, 'User3',03);
 INSERT INTO branch VALUES(4, 'User4',04);
 --rollback DELETE FROM branch WHERE id in (1,2,3,4);
+
+--changeset techgeeknext:insert-branch-employee-02
+INSERT INTO employee VALUES(5, 'User5');
+INSERT INTO branch VALUES(5, 'User4',05);
+--rollback DELETE FROM branch WHERE id in (1,2,3,4);
