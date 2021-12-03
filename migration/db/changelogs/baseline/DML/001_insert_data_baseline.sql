8408565de740f37494c927e98a872d4346848466
BaoZhe <175539358@qq.com>
2021-12-03 09:58:32 +0800

    liquibase test

--- migration/db/changelogs/baseline/DML/001_insert_data_baseline.sql
+++ migration/db/changelogs/baseline/DML/001_insert_data_baseline.sql
@@ -4,5 +4,4 @@ INSERT INTO employee VALUES(01, 'User1');
 INSERT INTO employee VALUES(02, 'User2');
 INSERT INTO employee VALUES(03, 'User3');
 INSERT INTO employee VALUES(04, 'User4');
-
 --rollback select 1;
